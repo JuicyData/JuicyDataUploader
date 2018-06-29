dd3664362837f6e7197289bd978db6494c6ba13a
CameronDeMille <camerondemille2153@gmail.com>
2018-06-28 23:24:31 -0700

    Updated UI a little

--- js/functions.js
+++ js/functions.js
@@ -11,7 +11,27 @@ document.getElementById("findPath").onclick = () => {
       alert("No folder was selected");
     } else {
       folderPath = fileNames[0];
-      document.getElementById("folderPath").value = folderPath;
+      fs.readFile(folderPath + "/matches.txt", "utf-8", (err, data) => {
+        if (err) {
+          alert(err + "\n\n Scoring system not found");
+          return;
+        }
+        if (folderPath === undefined) {
+          alert(
+            "No folder is selected. Please locate the folder that the scoring system is in"
+          );
+        } else {
+          document.getElementById("folderPath").value = folderPath;
+          document.getElementById("uploadScheduleButton").disabled = false;
+          document.getElementById("syncButton").disabled = false;
+          document
+            .getElementById("uploadScheduleButton")
+            .classList.replace("btn-secondary", "btn-primary");
+          document
+            .getElementById("syncButton")
+            .classList.replace("btn-secondary", "btn-success");
+        }
+      });
     }
   });
 };
@@ -67,7 +87,6 @@ document.getElementById("uploadScheduleButton").onclick = () => {
         _id: "1718-CAL-GAMES",
         schedule: y
       };
-      console.log(schedule);
     }
     axios
       .post("http://127.0.0.1:3000/api/data/uploadSchedule", schedule)
@@ -75,7 +94,7 @@ document.getElementById("uploadScheduleButton").onclick = () => {
         console.log(response);
       })
       .catch(function(error) {
-        console.log(error);
+        alert(error);
       });
   });
 };
@@ -106,177 +125,183 @@ document.getElementById("syncButton").onclick = () => {
       let gameData = [];
       let matchData = [];
       for (let i = 0; i < x.length; i++) {
-        if (x[i][29] == 1) { 
-        gameData.push({
-          _id: {
-            toaEventKey: "1718-CAL-GAMES",
-            matchInformation: {
-              matchNumber: x[i][2],
-              robotAlliance: "red",
-              teams: [x[i][5], x[i][6]]
-            }
-          },
-          gameInformation: {
-            auto: {
-              jewel: x[i][30],
-              glyphs: x[i][31],
-              keys: x[i][32],
-              park: x[i][33]
-            },
-            driver: {
-              glyphs: x[i][34],
-              rows: x[i][35],
-              columns: x[i][36],
-              cypher: x[i][37]
-            },
-            end: {
-              relic1: x[i][38],
-              relic2: x[i][39],
-              relic3: x[i][40],
-              relicsUp: x[i][41],
-              balanced: x[i][42]
-            }
-          }
-        });
-        gameData.push({
-          _id: {
-            toaEventKey: "1718-CAL-GAMES",
-            matchInformation: {
-              matchNumber: x[i][2],
-              robotAlliance: "blue",
-              teams: [x[i][8], x[i][9]]
-            }
-          },
-          gameInformation: {
-            auto: {
-              jewel: x[i][47],
-              glyphs: x[i][48],
-              keys: x[i][49],
-              park: x[i][50]
-            },
-            driver: {
-              glyphs: x[i][51],
-              rows: x[i][52],
-              columns: x[i][53],
-              cypher: x[i][54]
-            },
-            end: {
-              relic1: x[i][55],
-              relic2: x[i][56],
-              relic3: x[i][57],
-              relicsUp: x[i][58],
-              balanced: x[i][59]
-            }
-          }
-        });
-        redAuto =
-          Number(x[i][30]) * 30 +
-          Number(x[i][31]) * 15 +
-          Number(x[i][32]) * 30 +
-          Number(x[i][33]) * 10;
-        blueAuto =
-          Number(x[i][47]) * 30 +
-          Number(x[i][48]) * 15 +
-          Number(x[i][49]) * 30 +
-          Number(x[i][50]) * 10;
-        redDriver =
-          Number(x[i][34]) * 2 +
-          Number(x[i][35]) * 10 +
-          Number(x[i][36]) * 20 +
-          Number(x[i][37]) * 30;
-        blueDriver =
-          Number(x[i][51]) * 2 +
-          Number(x[i][52]) * 10 +
-          Number(x[i][53]) * 20 +
-          Number(x[i][54]) * 30;
-        redEnd =
-          Number(x[i][38]) * 10 +
-          Number(x[i][39]) * 20 +
-          Number(x[i][40]) * 40 +
-          Number(x[i][41]) * 15 +
-          Number(x[i][42]) * 20;
-        blueEnd =
-          Number(x[i][55]) * 10 +
-          Number(x[i][56]) * 20 +
-          Number(x[i][57]) * 40 +
-          Number(x[i][58]) * 15 +
-          Number(x[i][59]) * 20;
-        matchData.push({
-          _id: {
-            toaEventKey: "1718-CAL-GAMES",
-            matchInformation: {
-              matchNumber: x[i][2],
-              teams: {
-                red1: x[i][5],
-                red2: x[i][6],
-                blue1: x[i][8],
-                blue2: x[i][9]
+        if (x[i][29] == 1) {
+          gameData.push({
+            _id: {
+              toaEventKey: "1718-CAL-GAMES",
+              matchInformation: {
+                matchNumber: x[i][2],
+                robotAlliance: "red",
+                teams: [x[i][5], x[i][6]]
               }
-            }
-          },
-          resultInformation: {
-            score: {
+            },
+            gameInformation: {
               auto: {
-                red: redAuto,
-                blue: blueAuto
+                jewel: x[i][30],
+                glyphs: x[i][31],
+                keys: x[i][32],
+                park: x[i][33]
               },
               driver: {
-                red: redDriver,
-                blue: blueDriver
+                glyphs: x[i][34],
+                rows: x[i][35],
+                columns: x[i][36],
+                cypher: x[i][37]
               },
               end: {
-                red: redEnd,
-                blue: blueEnd
-              },
-              total: {
-                red: redAuto + redDriver + redEnd,
-                blue: blueAuto + blueDriver + blueEnd
-              },
-              penalty: {
-                red: x[i][62] * 10 + x[i][63] * 40,
-                blue: x[i][60] * 10 + x[i][61] * 40
-              },
-              final: {
-                red:
-                  (x[i][60] * 10) + (x[i][61] * 40) + redAuto + redDriver + redEnd,
-                blue:
-                  (x[i][62] * 10) +
-                  (x[i][63] * 40) +
-                  blueAuto +
-                  blueDriver +
-                  blueEnd
+                relic1: x[i][38],
+                relic2: x[i][39],
+                relic3: x[i][40],
+                relicsUp: x[i][41],
+                balanced: x[i][42]
+              }
+            }
+          });
+          gameData.push({
+            _id: {
+              toaEventKey: "1718-CAL-GAMES",
+              matchInformation: {
+                matchNumber: x[i][2],
+                robotAlliance: "blue",
+                teams: [x[i][8], x[i][9]]
               }
             },
-            winner:
-              x[i][60] * 10 + x[i][61] * 40 + redAuto + redDriver + redEnd ==
-              x[i][62] * 10 + x[i][63] * 40 + blueAuto + blueDriver + blueEnd
-                ? "tie"
-                : x[i][60] * 10 + x[i][61] * 40 + redAuto + redDriver + redEnd <
-                  x[i][62] * 10 +
+            gameInformation: {
+              auto: {
+                jewel: x[i][47],
+                glyphs: x[i][48],
+                keys: x[i][49],
+                park: x[i][50]
+              },
+              driver: {
+                glyphs: x[i][51],
+                rows: x[i][52],
+                columns: x[i][53],
+                cypher: x[i][54]
+              },
+              end: {
+                relic1: x[i][55],
+                relic2: x[i][56],
+                relic3: x[i][57],
+                relicsUp: x[i][58],
+                balanced: x[i][59]
+              }
+            }
+          });
+          redAuto =
+            Number(x[i][30]) * 30 +
+            Number(x[i][31]) * 15 +
+            Number(x[i][32]) * 30 +
+            Number(x[i][33]) * 10;
+          blueAuto =
+            Number(x[i][47]) * 30 +
+            Number(x[i][48]) * 15 +
+            Number(x[i][49]) * 30 +
+            Number(x[i][50]) * 10;
+          redDriver =
+            Number(x[i][34]) * 2 +
+            Number(x[i][35]) * 10 +
+            Number(x[i][36]) * 20 +
+            Number(x[i][37]) * 30;
+          blueDriver =
+            Number(x[i][51]) * 2 +
+            Number(x[i][52]) * 10 +
+            Number(x[i][53]) * 20 +
+            Number(x[i][54]) * 30;
+          redEnd =
+            Number(x[i][38]) * 10 +
+            Number(x[i][39]) * 20 +
+            Number(x[i][40]) * 40 +
+            Number(x[i][41]) * 15 +
+            Number(x[i][42]) * 20;
+          blueEnd =
+            Number(x[i][55]) * 10 +
+            Number(x[i][56]) * 20 +
+            Number(x[i][57]) * 40 +
+            Number(x[i][58]) * 15 +
+            Number(x[i][59]) * 20;
+          matchData.push({
+            _id: {
+              toaEventKey: "1718-CAL-GAMES",
+              matchInformation: {
+                matchNumber: x[i][2],
+                teams: {
+                  red1: x[i][5],
+                  red2: x[i][6],
+                  blue1: x[i][8],
+                  blue2: x[i][9]
+                }
+              }
+            },
+            resultInformation: {
+              score: {
+                auto: {
+                  red: redAuto,
+                  blue: blueAuto
+                },
+                driver: {
+                  red: redDriver,
+                  blue: blueDriver
+                },
+                end: {
+                  red: redEnd,
+                  blue: blueEnd
+                },
+                total: {
+                  red: redAuto + redDriver + redEnd,
+                  blue: blueAuto + blueDriver + blueEnd
+                },
+                penalty: {
+                  red: x[i][62] * 10 + x[i][63] * 40,
+                  blue: x[i][60] * 10 + x[i][61] * 40
+                },
+                final: {
+                  red:
+                    x[i][60] * 10 +
+                    x[i][61] * 40 +
+                    redAuto +
+                    redDriver +
+                    redEnd,
+                  blue:
+                    x[i][62] * 10 +
                     x[i][63] * 40 +
                     blueAuto +
                     blueDriver +
                     blueEnd
-                  ? "blue"
-                  : "red"
-          }
-        });
+                }
+              },
+              winner:
+                x[i][60] * 10 + x[i][61] * 40 + redAuto + redDriver + redEnd ==
+                x[i][62] * 10 + x[i][63] * 40 + blueAuto + blueDriver + blueEnd
+                  ? "tie"
+                  : x[i][60] * 10 +
+                      x[i][61] * 40 +
+                      redAuto +
+                      redDriver +
+                      redEnd <
+                    x[i][62] * 10 +
+                      x[i][63] * 40 +
+                      blueAuto +
+                      blueDriver +
+                      blueEnd
+                    ? "blue"
+                    : "red"
+            }
+          });
+        }
       }
-      }
-      console.log({gameData});
-      console.log({matchData})
       request = {
         gameData: gameData,
         matchData: matchData
-      }
+      };
       axios
-      .post("http://127.0.0.1:3000/api/data/uploadSync", request)
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+        .post("http://127.0.0.1:3000/api/data/uploadSync", request)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          alert(error);
+        });
     }
   });
 };
