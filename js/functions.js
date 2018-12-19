38db4254cc7b34d75ee145d736dbc3d8b15fbe53
CameronDeMille <camerondemille2153@gmail.com>
2018-12-19 13:31:18 -0800

    Updated for the new season

--- js/functions.js
+++ js/functions.js
@@ -4,240 +4,216 @@ var fs = require('fs');
 var axios = require('axios');
 
 // let folderPath;
+let ip = '';
 
 document.getElementById('uploadScheduleButton').onclick = () => {
+  let eventID = document.getElementById('eventID').value;
   axios
-    .get('http://localhost/apiv1/events/test_1/rankings/')
+    .get('http://localhost/apiv1/events/' + eventID + '/matches/')
     .then(function(response) {
       console.log(response);
+      scoringSystemSchedule = response.data.matches;
+      tempSchedule = [];
+      for (let i = 0; i < scoringSystemSchedule.length; i++) {
+        tempSchedule[i] = {
+          matchNumber: scoringSystemSchedule[i].matchNumber,
+          teams: {
+            red1: {
+              teamNumber: scoringSystemSchedule[i].red.team1,
+              surrogate: scoringSystemSchedule[i].red.isTeam1Surrogate
+            },
+            red2: {
+              teamNumber: scoringSystemSchedule[i].red.team2,
+              surrogate: scoringSystemSchedule[i].red.isTeam2Surrogate
+            },
+            blue1: {
+              teamNumber: scoringSystemSchedule[i].blue.team1,
+              surrogate: scoringSystemSchedule[i].blue.isTeam1Surrogate
+            },
+            blue2: {
+              teamNumber: scoringSystemSchedule[i].blue.team2,
+              surrogate: scoringSystemSchedule[i].blue.isTeam2Surrogate
+            }
+          }
+        };
+      }
+      schedule = {
+        _id: eventID,
+        schedule: tempSchedule
+      };
+      console.log({schedule});
+      axios
+        .post('http://' + ip + '/api/events/ftc/event/uploadSchedule', schedule)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          alert(error);
+        });
     })
     .catch(function(error) {
       alert(error);
     });
 };
 
-// document.getElementById("findPath").onclick = () => {
-//   dialog.showOpenDialog({ properties: ["openDirectory"] }, fileNames => {
-//     if (fileNames === undefined) {
-//       alert("No folder was selected");
-//     } else {
-//       folderPath = fileNames[0];
-//       fs.readFile(folderPath + "/matches.txt", "utf-8", (err, data) => {
-//         if (err) {
-//           alert(err + "\n\n Scoring system not found");
-//           return;
-//         }
-//         if (folderPath === undefined) {
-//           alert(
-//             "No folder is selected. Please locate the folder that the scoring system is in"
-//           );
-//         } else {
-//           document.getElementById("folderPath").value = folderPath;
-//           document.getElementById("uploadScheduleButton").disabled = false;
-//           document.getElementById("syncButton").disabled = false;
-//           document
-//             .getElementById("uploadScheduleButton")
-//             .classList.replace("btn-secondary", "btn-primary");
-//           document
-//             .getElementById("syncButton")
-//             .classList.replace("btn-secondary", "btn-success");
-//         }
-//       });
-//     }
-//   });
-// };
+document.getElementById('syncButton').onclick = () => {
+  let eventID = document.getElementById('eventID').value;
+  axios
+    .get('http://localhost/apiv1/events/' + eventID + '/matches/')
+    .then(function(response) {
+      scoringSystemSchedule = response.data.matches;
+      tempSchedule = [];
+      for (let i = 0; i < scoringSystemSchedule.length; i++) {
+        if (scoringSystemSchedule[i].finished) {
+          tempSchedule.push({
+            matchNumber: scoringSystemSchedule[i].matchNumber,
+            teams: {
+              red1: scoringSystemSchedule[i].red.team1,
+              red2: scoringSystemSchedule[i].red.team2,
+              blue1: scoringSystemSchedule[i].blue.team1,
+              blue2: scoringSystemSchedule[i].blue.team2
+            }
+          });
+        }
+      }
+      matchData = [];
+      gameData = [];
+      for (let i = 0; i < tempSchedule.length; i++) {
+        axios
+          .get(
+            'http://localhost/apiv1/2019/events/' +
+              eventID +
+              '/matches/' +
+              tempSchedule[i].matchNumber
+          )
+          .then(function(response) {
+            response = response.data;
+            matchData[i] = {
+              matchInformation: {
+                matchNumber: tempSchedule[i].matchNumber,
+                teams: tempSchedule[i].teams
+              },
+              resultInformation: {
+                score: {
+                  penalty: {
+                    red: response.blue.penalty,
+                    blue: response.red.penalty
+                  }
+                }
+              }
+            };
+            bluePartial = 0;
+            blueFull = 0;
+            redPartial = 0;
+            redFull = 0;
+            if (response.blue.endParking == 15) {
+              bluePartial = 1;
+            }
+            if (response.blue.endParking == 25) {
+              blueFull = 1;
+            }
+            if (response.blue.endParking == 30) {
+              bluePartial = 2;
+            }
+            if (response.blue.endParking == 40) {
+              blueFull = 1;
+              bluePartial = 1;
+            }
+            if (response.red.endParking == 50) {
+              blueFull = 2;
+            }
 
-// document.getElementById("uploadScheduleButton").onclick = () => {
-//   fs.readFile(folderPath + "/matches.txt", "utf-8", (err, data) => {
-//     if (err) {
-//       alert(err);
-//       return;
-//     }
+            if (response.red.endParking == 15) {
+              redPartial = 1;
+            }
+            if (response.red.endParking == 25) {
+              redFull = 1;
+            }
+            if (response.red.endParking == 30) {
+              redPartial = 2;
+            }
+            if (response.red.endParking == 40) {
+              redFull = 1;
+              redPartial = 1;
+            }
+            if (response.red.endParking == 50) {
+              redFull = 2;
+            }
 
-//     if (folderPath === undefined) {
-//       alert(
-//         "No folder is selected. Please locate the folder that the scoring system is in"
-//       );
-//     } else {
-//       data = data.replace("", "");
-//       let x = data.split("\n");
-//       for (let i = 0; i < x.length; i++) {
-//         if (x[i] === "") {
-//           x.splice(i, i + 1);
-//         }
-//       }
-//       for (let i = 0; i < x.length; i++) {
-//         x[i] = x[i].split("|");
-//       }
-//       let y = [];
-//       for (let i = 0; i < x.length; i++) {
-//         y.push({
-//           matchNumber: Number(i + 1),
-//           teams: {
-//             red1: {
-//               teamNumber: Number(x[i][5]),
-//               surrogate: x[i][23] == 1
-//             },
-//             red2: {
-//               teamNumber: Number(x[i][6]),
-//               surrogate: x[i][24] == 1
-//             },
-//             blue1: {
-//               teamNumber: Number(x[i][8]),
-//               surrogate: x[i][26] == 1
-//             },
-//             blue2: {
-//               teamNumber: Number(x[i][9]),
-//               surrogate: x[i][25] == 1
-//             }
-//           }
-//         });
-//       }
-//       schedule = {
-//         _id: "baka",
-//         schedule: y
-//       };
-//     }
-//     console.log({ schedule });
-//     axios
-//       .post(
-//         "http://" +
-//           document.getElementById("api").value +
-//           "/api/data/uploadSchedule",
-//         schedule
-//       )
-//       .then(function(response) {
-//         console.log(response);
-//       })
-//       .catch(function(error) {
-//         alert(error);
-//       });
-//   });
-// };
-
-// document.getElementById("syncButton").onclick = () => {
-//   fs.readFile(folderPath + "/matches.txt", "utf-8", (err, data) => {
-//     if (err) {
-//       alert(err);
-//       return;
-//     }
-
-//     if (folderPath === undefined) {
-//       alert(
-//         "No folder is selected. Please locate the folder that the scoring system is in"
-//       );
-//     } else {
-//       data = data.replace("", "");
-//       let x = data.split("\n");
-//       for (let i = 0; i < x.length; i++) {
-//         if (x[i] === "") {
-//           x.splice(i, i + 1);
-//         }
-//       }
-//       for (let i = 0; i < x.length; i++) {
-//         x[i] = x[i].split("|");
-//       }
-//       let gameData = [];
-//       let matchData = [];
-//       for (let i = 0; i < x.length; i++) {
-//         if (x[i][29] == 1) {
-//           gameData.push({
-//             matchInformation: {
-//               matchNumber: Number(x[i][2]),
-//               robotAlliance: "red",
-//               teams: [Number(x[i][5]), Number(x[i][6])]
-//             },
-//             auto: {
-//               jewel: Number(x[i][30]),
-//               glyphs: Number(x[i][31]),
-//               keys: Number(x[i][32]),
-//               park: Number(x[i][33])
-//             },
-//             driver: {
-//               glyphs: Number(x[i][34]),
-//               rows: Number(x[i][35]),
-//               columns: Number(x[i][36]),
-//               cypher: Number(x[i][37])
-//             },
-//             end: {
-//               relic1: Number(x[i][38]),
-//               relic2: Number(x[i][39]),
-//               relic3: Number(x[i][40]),
-//               relicsUp: Number(x[i][41]),
-//               balanced: Number(x[i][42])
-//             }
-//           });
-//           gameData.push({
-//             matchInformation: {
-//               matchNumber: Number(x[i][2]),
-//               robotAlliance: "blue",
-//               teams: [Number(x[i][8]), Number(x[i][9])]
-//             },
-//             auto: {
-//               jewel: Number(x[i][47]),
-//               glyphs: Number(x[i][48]),
-//               keys: Number(x[i][49]),
-//               park: Number(x[i][50])
-//             },
-//             driver: {
-//               glyphs: Number(x[i][51]),
-//               rows: Number(x[i][52]),
-//               columns: Number(x[i][53]),
-//               cypher: Number(x[i][54])
-//             },
-//             end: {
-//               relic1: Number(x[i][55]),
-//               relic2: Number(x[i][56]),
-//               relic3: Number(x[i][57]),
-//               relicsUp: Number(x[i][58]),
-//               balanced: Number(x[i][59])
-//             }
-//           });
-//           matchData.push({
-//             matchInformation: {
-//               matchNumber: Number(x[i][2]),
-//               teams: {
-//                 red1: Number(x[i][5]),
-//                 red2: Number(x[i][6]),
-//                 blue1: Number(x[i][8]),
-//                 blue2: Number(x[i][9])
-//               }
-//             },
-//             resultInformation: {
-//               score: {
-//                 penalty: {
-//                   red: Number(x[i][62] * 10 + x[i][63] * 40),
-//                   blue: Number(x[i][60] * 10 + x[i][61] * 40)
-//                 }
-//               }
-//             }
-//           });
-//         }
-//       }
-//       request = {
-//         seasonId: {
-//           season: '2017-2018',
-//           first: 'ftc'
-//         },
-//         eventKey: 'baka',
-//         gameData: gameData,
-//         matchData: matchData
-//       };
-
-//       console.log({ request });
-//       axios
-//         .post(
-//           "http://" +
-//             document.getElementById("api").value +
-//             "/api/data/uploadSync",
-//           request
-//         )
-//         .then(function(response) {
-//           console.log(response);
-//         })
-//         .catch(function(error) {
-//           alert(error);
-//         });
-//     }
-//   });
-// };
+            gameData.push({
+              matchInformation: {
+                matchNumber: tempSchedule[i].matchNumber,
+                robotAlliance: 'blue',
+                teams: [
+                  tempSchedule[i].teams.blue1,
+                  tempSchedule[i].teams.blue2
+                ]
+              },
+              auto: {
+                landing: response.blue.landed / 30,
+                sampling: response.blue.mineralSample / 25,
+                claiming: response.blue.claimedDepot / 15,
+                parking: response.blue.autoParking / 10
+              },
+              driver: {
+                goldMineral: response.blue.landerGold / 5,
+                silverMineral: response.blue.landerSilver / 5,
+                anyMineral: response.blue.depotMinerals / 2
+              },
+              end: {
+                latched: response.blue.latchedLander / 50,
+                parkedCrater: bluePartial,
+                parkedCompletelyCrater: blueFull
+              }
+            });
+            gameData.push({
+              matchInformation: {
+                matchNumber: tempSchedule[i].matchNumber,
+                robotAlliance: 'red',
+                teams: [tempSchedule[i].teams.red1, tempSchedule[i].teams.red2]
+              },
+              auto: {
+                landing: response.red.landed / 30,
+                sampling: response.red.mineralSample / 25,
+                claiming: response.red.claimedDepot / 15,
+                parking: response.red.autoParking / 10
+              },
+              driver: {
+                goldMineral: response.red.landerGold / 5,
+                silverMineral: response.red.landerSilver / 5,
+                anyMineral: response.red.depotMinerals / 2
+              },
+              end: {
+                latched: response.red.latchedLander / 50,
+                parkedCrater: redPartial,
+                parkedCompletelyCrater: redFull
+              }
+            });
+            request = {
+              seasonId: {
+                season: '2018-2019',
+                first: 'ftc'
+              },
+              eventKey: eventID,
+              gameData: gameData,
+              matchData: matchData
+            };
+            console.log({request})
+            axios
+              .post(
+                'http://' + ip + '/api/events/ftc/event/uploadSync',
+                request
+              )
+              .then(function(response) {
+                console.log(response);
+              })
+              .catch(function(error) {
+                alert(error);
+              });
+          })
+          .catch(function(error) {
+            alert(error);
+          });
+      }
+    });
+};
